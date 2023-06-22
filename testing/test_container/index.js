fa937ef660656c0adb5b044d9df4f36c2083a58e
roman huesler <roman.huesler@gmx.ch>
2023-06-22 05:57:30 +0200

    fix: testcontainer listen on all hostnames

--- testing/test_container/index.js
+++ testing/test_container/index.js
@@ -40,5 +40,5 @@ const server = http.createServer((req, res) => {
 
 const port = 3000;
 server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://0.0.0.0:${port}/`);
 });
