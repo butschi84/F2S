ca244396e363e80d3eae796b7154eb67f6a7a879
roman huesler <roman.huesler@gmx.ch>
2023-08-21 13:33:38 +0200

    chore: testcontainer try fix syntax

--- testing/container_nodejs_test/init.sh
+++ testing/container_nodejs_test/init.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# Start the first process
 npm start &
-
-# Start the second process
 ./f2sfizzlet &
 
-# Wait for any process to exit
+# wait for any process to exit
 wait -n
 
-# Exit with status of process that exited first
+# exit with status of process that exited first
 exit $?
